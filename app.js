88e19adbdb74d1e7d8dd9f92cfb8d8d1e64fb51d
Mahdy Muzzammil <156871399+HeyMahdy@users.noreply.github.com>
2025-03-14 01:57:25 +0600

    init

---
+++ app.js
@@ -0,0 +1,41 @@
+
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import logger from "./logger.js";
+import morgan from "morgan";
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+
+const morganFormat = ":method :url :status :response-time ms";
+
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
+
+
+
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+    });
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
